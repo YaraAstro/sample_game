c7267c0778e1917f6a0a60a6a1bfcc1cca85fb51
Yara Astro <93931829+YaraAstro@users.noreply.github.com>
2024-10-01 20:38:10 +0530

    Delta Time

--- main.py
+++ main.py
@@ -107,10 +107,11 @@ class MainWidget(Widget):
         return int(tr_x), int(tr_y)
     
     def update (self, dt):
-        # print("Update")
+        # print("delta time : " + str(dt) + "- 1/60 : " + str(1.0/60.0))
+        time_factor = dt * 60
         self.update_vertical_lines()
         self.update_horizontal_lines()
-        self.current_offset_y += self.SPEED
+        self.current_offset_y += self.SPEED * time_factor
 
         spacing_y = self.H_LINE_SPACING * self.height
         if self.current_offset_y >= spacing_y:
