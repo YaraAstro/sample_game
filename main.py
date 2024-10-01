edfa94a12dd46e16b73cbccaba4c91be0c7cf593
Yara Astro <93931829+YaraAstro@users.noreply.github.com>
2024-10-01 20:57:56 +0530

    Exercise : Key Press

--- main.py
+++ main.py
@@ -24,7 +24,8 @@ class MainWidget(Widget):
     SPEED = 3
     current_offset_y = 0
     
-    SPEED_X = 1
+    SPEED_X = 10
+    current_speed_x = 0
     current_offset_x = 0
 
     def __init__(self, **kwargs):
@@ -114,6 +115,18 @@ class MainWidget(Widget):
 
         return int(tr_x), int(tr_y)
     
+    def on_touch_down(self, touch):
+        if touch.x < self.width / 2:
+            print("<- to left")
+            self.current_speed_x = self.SPEED_X
+        else:
+            print("-> to right")
+            self.current_speed_x = -self.SPEED_X
+    
+    def on_touch_up(self, touch):
+        print("UP^")
+        self.current_speed_x = 0
+    
     def update (self, dt):
         # print("delta time : " + str(dt) + "- 1/60 : " + str(1.0/60.0))
         time_factor = dt * 60
@@ -124,7 +137,7 @@ class MainWidget(Widget):
         spacing_y = self.H_LINE_SPACING * self.height
         if self.current_offset_y >= spacing_y:
             self.current_offset_y -= spacing_y
-        self.current_offset_x += self.SPEED_X * time_factor
+        self.current_offset_x += self.current_speed_x * time_factor
 
 
 class GalaxyApp (App):
