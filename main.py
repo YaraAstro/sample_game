cffba6a384e5f9e21d674dd5b8330e8de20c94ae
Yara Astro <93931829+YaraAstro@users.noreply.github.com>
2024-10-01 20:48:08 +0530

    Exercise : Lateral Movement

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+from kivy.config import Config
+
+Config.set('graphics', 'width', '900')
+Config.set('graphics', 'height', '400')
+
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.properties import NumericProperty, Clock
@@ -16,8 +21,11 @@ class MainWidget(Widget):
     H_LINE_SPACING = .1 # percentage in screen height
     horizontal_lines = []
     
-    SPEED = 1
+    SPEED = 3
     current_offset_y = 0
+    
+    SPEED_X = 1
+    current_offset_x = 0
 
     def __init__(self, **kwargs):
         super(MainWidget, self).__init__(**kwargs)
@@ -58,7 +66,7 @@ class MainWidget(Widget):
         spacing = int(self.V_LINE_SPACING * self.width)
         # self.line.points = [center_x, 0, center_x, 100]
         for i in range(0, self.V_NB_LINES):
-            line_x = center_line_x + offset * spacing
+            line_x = center_line_x + (offset * spacing) + self.current_offset_x 
             x1, y1 = self.transform(line_x, 0)
             x2, y2 = self.transform(line_x, self.height)
             self.vertical_lines[i].points = [x1, y1, x2, y2]
@@ -75,8 +83,8 @@ class MainWidget(Widget):
         center_line_y = int(self.width / 2)
         offset = int(self.V_NB_LINES / 2) - 0.5
         spacing = int(self.V_LINE_SPACING * self.width)
-        x_min = center_line_y - offset * spacing
-        x_max = center_line_y + offset * spacing
+        x_min = center_line_y - (offset * spacing) + self.current_offset_x
+        x_max = center_line_y + (offset * spacing) + self.current_offset_x
         spacing_y = self.H_LINE_SPACING * self.height 
         for i in range(0, self.H_NB_LINES):
             line_y = i * spacing_y - self.current_offset_y
@@ -116,6 +124,7 @@ class MainWidget(Widget):
         spacing_y = self.H_LINE_SPACING * self.height
         if self.current_offset_y >= spacing_y:
             self.current_offset_y -= spacing_y
+        self.current_offset_x += self.SPEED_X * time_factor
 
 
 class GalaxyApp (App):
